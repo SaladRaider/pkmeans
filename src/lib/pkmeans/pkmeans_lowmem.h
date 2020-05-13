0f3ebe2c2f6492735c67f457151d4fa057165fa7
Peter Veerman <peterkangveerman@gmail.com>
2020-05-12 18:48:19 -0700

    Add a low-mem option that does not optimize using the triangle inequality.

---
+++ src/lib/pkmeans/pkmeans_lowmem.h
@@ -0,0 +1,36 @@
+#ifndef __PKMEANS_LOWMEM_H_INCLUDED_
+#define __PKMEANS_LOWMEM_H_INCLUDED_
+
+#include <gtest/gtest.h>
+#include <pkmeans/distribution.h>
+#include <string>
+
+namespace pkmeans {
+class PKMeansLowMem {
+    private:
+        std::vector<Distribution<float>> clusters;
+        std::vector<Distribution<float>> distributions;
+        std::vector<std::vector<size_t>> clusterAssignments;
+        std::vector<size_t> clusterMap;
+        bool converged = false;
+
+        void readDistributions (std::string inFileName);
+        void saveClusters (std::string outFilename);
+        void saveAssignments (std::string outFilename);
+        void initClusters (int numClusters);
+        void clearClusterAssignments ();
+        void assignDistributions ();
+        void initAssignments ();
+        void computeNewClusters ();
+        void computeClusterMean (size_t c);
+        float calcObjFn ();
+        size_t findClosestCluster (size_t x);
+        size_t findClosestInitCluster (size_t x);
+        size_t getCluster (size_t x);
+    public:
+        void run (int numClusters, int numThreads, std::string inFilename,
+                  std::string assignmentsOutFilename, std::string clustersOutFilename);
+};
+}
+
+#endif // __PKMEANS_LOWMEM_H_INCLUDED_
